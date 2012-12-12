c4a9277d651ef619691d2f6a155241c2e7f77bfd
Rex Feng <rexfeng@gmail.com>
2012-12-12 17:49:20 -0500

    initial commit

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.8'
+gem 'pg'
+
+# gem 'omniauth-foursquare'
+# gem 'httparty'
+# gem 'kaminari'
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :development, :test do
+  gem 'rspec-rails'
+  gem 'capybara'
+  gem 'debugger'
+  gem 'guard-rspec'
+  gem 'terminal-notifier-guard'
+  gem 'awesome_print'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geographapp::Application.config.secret_token = '8fe57b24bd5b43630bec1ce3a9fc25ee9118848becc0343418e3687dd263565c0fed291b9d8aa939532a2a8e4f43dc8c7627402bc0e0629f99c71f13be8c9aa6'
