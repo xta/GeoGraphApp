62641bbbf84ae2d146ca08c9ae8727d146e87d9a
Rex Feng <rexfeng@gmail.com>
2013-01-01 20:18:41 -0800

    update log testing

--- spec/models/log_spec.rb
+++ spec/models/log_spec.rb
@@ -2,9 +2,26 @@ require 'spec_helper'
 
 describe Log do
 
-  describe '.sparklines' do
+  # before(:each) do
+  #   #create user
+  #   # let(:user) { User.find_by_uid('12345678') }
+  #   #setup client
+  # end
 
-    it 'should display sparklines'
+  context 'for current user' do
+
+    describe '#store_checkins' do
+
+      it 'accepts an array of checkins and saves it as a log' do
+
+
+      end
+
+    end
+
+    # describe '#sparklines' do
+    #   it 'displays sparklines'
+    # end
 
   end
 
